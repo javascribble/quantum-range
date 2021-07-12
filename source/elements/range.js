b8bb3159b52ba3913763894d7daa30b333737d75
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:59:29 -0400

    update build

--- source/elements/range.js
+++ source/elements/range.js
@@ -1,5 +1,9 @@
+import range from '../templates/range.js';
+
 export class Range extends Quantum {
     constructor() {
         super();
     }
 }
+
+Range.define('quantum-range', range);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Range } from './elements/range.js';
-import range from './templates/range.js';
-
-Range.define('quantum-range', range);
